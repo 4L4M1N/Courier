dcde0865105c31460d923effca800e7cd5509558
4L4M1N <mohammadamin601@gmail.com>
2019-10-18 15:12:44 +0600

    Role class added

---
+++ CourierAPI/Models/Role.cs
@@ -0,0 +1,11 @@
+namespace CourierAPI.Models
+{
+    public class Role
+    {
+        public const string SuperAdmin = "SuperAdmin";
+        public const string Marchant = "Marchant";
+        public const string BookingOfficer = "BookingOfficer";
+        public const string DelivaryMan = "DelivaryMan";
+        public const string Client = "Client";
+    }
+}
