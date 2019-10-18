7f8fc27c132cca5940a0e5b5e30ad704c6afa628
4L4M1N <mohammadamin601@gmail.com>
2019-10-18 23:48:18 +0600

    add a role

--- CourierAPI/Models/Role.cs
+++ CourierAPI/Models/Role.cs
@@ -3,6 +3,7 @@ namespace CourierAPI.Models
     public class Role
     {
         public const string SuperAdmin = "SuperAdmin";
+        public const string CourierOwner = "CourierOwner";
         public const string Marchant = "Marchant";
         public const string BookingOfficer = "BookingOfficer";
         public const string DelivaryMan = "DelivaryMan";
