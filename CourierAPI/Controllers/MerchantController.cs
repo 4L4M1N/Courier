238f5624aaec261b474fabf01bb8382903e18e4d
4L4M1N <mohammadamin601@gmail.com>
2019-10-25 23:01:05 +0600

    edit merchant details & get all merchant

--- CourierAPI/Controllers/MerchantController.cs
+++ CourierAPI/Controllers/MerchantController.cs
@@ -15,9 +15,7 @@ namespace CourierAPI.Controllers
     public class MerchantController : ControllerBase
     {
         
-        private readonly DataContext _context;
         private readonly IUnitOfWork _unitOfWork;
-
         public MerchantController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
@@ -33,8 +31,8 @@ namespace CourierAPI.Controllers
         [HttpPost("create")]
         public async Task<IActionResult> Create(MerchantToAddDTO addMerchant)
         {
-            var isMerchantNameExist = await _unitOfWork.Merchants.FindByMerchantNameAsync(addMerchant.Name);
-            if(isMerchantNameExist != null)
+            var isMerchantExist = await _unitOfWork.Merchants.FindByMerchantNameAsync(addMerchant.Name);
+            if(isMerchantExist != null)
                 return BadRequest("This name already exists!");
 
             var merchantToAdd = new Merchant
@@ -46,11 +44,39 @@ namespace CourierAPI.Controllers
                 TradeLicenseNo = addMerchant.TradeLicenseNo
             };
             await _unitOfWork.Merchants.AddMerchantAsync(merchantToAdd);
-            var result = await _unitOfWork.Complete();
+            var result = await _unitOfWork.CompleteAsync();
             if(result == 0) return BadRequest();
            
            return Ok();
             
+        }
+        [HttpPost("update")]
+        public async Task<IActionResult> Update(MerchantToAddDTO updateMerchant)
+        {
+            var isMerchantExist = await _unitOfWork.Merchants.FindByMerchantNameAsync(updateMerchant.Name);
+            if(isMerchantExist == null) return BadRequest("User Not Found");
+
+            isMerchantExist.Email = updateMerchant.Email;
+            isMerchantExist.BankAccountNo = updateMerchant.BankAccountNo;
+            isMerchantExist.Address = updateMerchant.Address;
+            isMerchantExist.Phone = updateMerchant.Phone;
+            isMerchantExist.TradeLicenseNo = updateMerchant.TradeLicenseNo;
+            var result = await _unitOfWork.CompleteAsync();
+            if(result == 0) return BadRequest();
+            return Ok("Merchant Updated");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMerchant(string id)
+        {
+            var isMerchantExist = await _unitOfWork.Merchants.GetMerchantDetailsAsync(id);
+            if(isMerchantExist == null) return BadRequest("Merchant not Found");
+            return Ok(isMerchantExist);
+        }
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllMerchants()
+        {
+            var result = await _unitOfWork.Merchants.GetMerchantsAsync();
+            return Ok(result);
         }
     }
 }
