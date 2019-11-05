179550f677d6d9347829fe64e3b8c6d42e4b3655
4L4M1N <mohammadamin601@gmail.com>
2019-11-05 23:58:42 +0600

    migration Merchant table

--- CourierAPI/Migrations/20191104152306_AddPropertyToMerchant.Designer.cs
+++ CourierAPI/Migrations/20191104152306_AddPropertyToMerchant.Designer.cs
@@ -4,14 +4,16 @@ using CourierAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CourierAPI.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20191104152306_AddPropertyToMerchant")]
+    partial class AddPropertyToMerchant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -33,9 +35,6 @@ namespace CourierAPI.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Email")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("MerchantIdentity")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
---
+++ CourierAPI/Migrations/20191105164521_AddMerchantIdToMerchant.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourierAPI.Migrations
+{
+    public partial class AddMerchantIdToMerchant : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MerchantId",
+                table: "Merchants",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MerchantId",
+                table: "Merchants");
+        }
+    }
+}
