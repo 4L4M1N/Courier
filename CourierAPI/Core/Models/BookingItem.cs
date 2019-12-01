2d91ab7cd7d32c4257b95a7ec1e1d80215c362be
4L4M1N <mohammadamin601@gmail.com>
2019-12-01 22:58:15 +0600

    delivMan, bookingitem added

---
+++ CourierAPI/Core/Models/BookingItem.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CourierAPI.Core.Models
+{
+    public class BookingItem
+    {
+        public int Id { get; set; }
+        public int ItemAttributeId { get; set; }
+        public string BookingId { get; set; }
+        [ForeignKey("BookingId")]
+        public Booking Booking { get; set; }
+        
+    }
+}
---
+++ CourierAPI/Infrastructure/Migrations/20191201154104_BookingItemsDelivManAdded.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourierAPI.Infrastructure.Migrations
+{
+    public partial class BookingItemsDelivManAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BookingDate",
+                table: "Bookings",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeliveredDate",
+                table: "Bookings",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Bookings",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AssignedDelivMen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookingId = table.Column<string>(nullable: true),
+                    DelivManId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignedDelivMen", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssignedDelivMen_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemAttributeId = table.Column<int>(nullable: false),
+                    BookingId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookingItems_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeliveryMen",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DelivManIdentity = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeliveryMen", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignedDelivMen_BookingId",
+                table: "AssignedDelivMen",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingItems_BookingId",
+                table: "BookingItems",
+                column: "BookingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AssignedDelivMen");
+
+            migrationBuilder.DropTable(
+                name: "BookingItems");
+
+            migrationBuilder.DropTable(
+                name: "DeliveryMen");
+
+            migrationBuilder.DropColumn(
+                name: "BookingDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveredDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Bookings");
+        }
+    }
+}
