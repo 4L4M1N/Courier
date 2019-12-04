39d5b984f9d322c7284194ded30a8571c3bfaf4b
4L4M1N <mohammadamin601@gmail.com>
2019-12-04 20:28:01 +0600

    deliv password

--- CourierAPI/Core/Models/DeliveryMan.cs
+++ CourierAPI/Core/Models/DeliveryMan.cs
@@ -6,5 +6,7 @@ namespace CourierAPI.Core.Models
         public string DelivManIdentity  { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
---
+++ CourierAPI/Infrastructure/Migrations/20191204122131_passToDelivaryMan.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourierAPI.Infrastructure.Migrations
+{
+    public partial class passToDelivaryMan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "DeliveryMen",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "DeliveryMen",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ShowBookings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: true),
+                    BookingDate = table.Column<DateTime>(nullable: false),
+                    ReceiverName = table.Column<string>(nullable: true),
+                    DelivManName = table.Column<string>(nullable: true),
+                    Zone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShowBookings");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "DeliveryMen");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "DeliveryMen");
+        }
+    }
+}
